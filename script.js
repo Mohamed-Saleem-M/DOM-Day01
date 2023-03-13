551c2b95a21f85945094adb7af5fc8be848f876f
Mohamed-Saleem-M <mohamedsaleem.mep@gmail.com>
2023-03-13 12:15:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function foo(){
+    var firstName = document.getElementById("firstName").value;
+    console.log(firstName);
+
+    var middleName = document.getElementById("middleName").value;
+    console.log(middleName);
+
+    var lastName = document.getElementById("lastName").value;
+    console.log(lastName);
+
+    var email = document.getElementById("email").value;
+    console.log(email);
+
+    var password = document.getElementById("password").value;
+    console.log(password);
+}
